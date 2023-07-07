3a19758575fb0639d9695579c7eb4a6b460ecb32
Wylunaa <wylunaa@gmail.com>
2023-07-07 16:06:00 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
-const mario = document.querySelector('.mario')
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const audioJump = document.querySelector('.audioJump'); 
+const gameOver = document.querySelector('.gameover');
 
-const jump = () {
+const jump =() => 
+{
     mario.classList.add('jump');
+    
+     audioJump.currentTime = 0.1;
+     audioJump.volume = 0.1;
+     audioJump.play();
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
 }
 
-document.addEventListener('keydown' , jump);  
+const loop = setInterval(() =>{
+
+    const pipePosition = pipe.offsetLeft;
+    
+    if (pipePosition <= 120) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        gameOver.currentTime = 0.1;
+        gameOver.volume = 0.2;
+        gameOver.play();
+    }
+}, 10);
+document.addEventListener('keydown', jump);
